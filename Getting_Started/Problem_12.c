7ec9e60feca4ae568ba8bef7fdad4037da1bc364
mohdayaan187 <mohdayaan0601@gmail.com>
2026-06-13 07:07:06 +0530

    Sum of digits

---
+++ Getting_Started/Problem_12.c
@@ -0,0 +1,30 @@
+//To calculate Sum of digits of a 5-digit number//
+
+#include <stdio.h>
+int main()
+{
+    int num, sum;
+    int d1, d2, d3, d4, d5;
+    
+    printf("Enter a 5-digit number: ");
+    scanf("%d", &num);
+
+    d5 = num % 10;
+    num = num / 10;
+    
+    d4 = num % 10;
+    num = num / 10;
+
+    d3 = num % 10;
+    num = num / 10;
+
+    d2 = num % 10;
+    num = num / 10; 
+
+    d1 = num % 10; 
+    
+    sum = d1 + d2 + d3 + d4 + d5;
+    printf("Sum of digits = %d", sum);
+
+    return 0;
+}
