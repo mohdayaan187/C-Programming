8b006b4bcb66037b7be93ef139c490c6622c2e7d
mohdayaan187 <mohdayaan0601@gmail.com>
2026-06-13 07:08:38 +0530

    Cartesian to Polar coordinates

---
+++ Getting_Started/Problem_13.c
@@ -0,0 +1,19 @@
+//To convert cartesian coordinates to polar coordinates//
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+int main()
+{
+    float x, y, r, theta;
+    printf("Enter the cartesian coordinates (x and y): ");
+    scanf("%f %f", &x, &y);
+    
+    r = sqrt(x * x + y * y); 
+    theta = atan(y / x) * (180 / PI); 
+
+    printf("Polar coordinates (r, theta) = (%.2f, %.2f degrees)", r, theta);
+    
+    return 0;
+}
