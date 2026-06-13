df9fb86eac5f0692cd99026d67d979824ff4d1a8
mohdayaan187 <mohdayaan0601@gmail.com>
2026-06-13 07:35:26 +0530

    Odd or Even

---
+++ Decision_Control/Problem_4.c
@@ -0,0 +1,16 @@
+//To find whether a number is even or odd//
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+        printf("%d is an even number.", num);
+    else
+        printf("%d is an odd number.", num);
+
+    return 0;
+}
