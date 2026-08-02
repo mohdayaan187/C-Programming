90687e258fd482b690bf057a13f79de8eb7cd683
Mohd Ayaan <mohdayaan0601@gmail.com>
2026-08-02 20:19:34 +0530

    Ten natural numbers using loop

---
+++ Loop_Control/Problem_1.c
@@ -0,0 +1,16 @@
+//To print first ten natural numbers//
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("First ten natural numbers are: \n");
+    i=1;
+
+    while (i<=10)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
