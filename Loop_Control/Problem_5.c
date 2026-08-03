212c7e9a624bfbcea715e6afc45e65d173aa53b4
Mohd Ayaan <mohdayaan0601@gmail.com>
2026-08-03 23:32:42 +0530

    ASCII values & equivalent characters

---
+++ Loop_Control/Problem_5.c
@@ -0,0 +1,13 @@
+//To print all ASCII values and their equivalent characters//
+
+#include <stdio.h>
+int main()
+{
+    int i=0;
+    while (i<=255)
+    {
+        printf("%d -> %c \n", i, i);
+        i++;
+    }
+    return 0;
+}
