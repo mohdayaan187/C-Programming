01ecedef85c5bef694728f0532af2111990d3e11
Mohd Ayaan <mohdayaan0601@gmail.com>
2026-08-09 11:41:50 +0530

    Natural logarithm by series summation

---
+++ Loop_Control/Problem_21.c
@@ -0,0 +1,23 @@
+//To find approximate value of natural logarithm by calculating first seven terms of the series://
+//(x-1)/x + 1/2(x-1/x)^2 + 1/2(x-1/x)^3 + .....//
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int i;
+    float x, y;
+    printf("Enter vaue of x: \n");
+    scanf("%f", &x);
+
+    y = (x - 1)/x;
+    float sum = y;
+
+    for(i=2; i<=20; i++)
+    {
+        sum = sum + ((1.0/i)*pow(y,i));
+    }
+    printf("Sum = %.4f", sum);
+
+    return 0;
+}
